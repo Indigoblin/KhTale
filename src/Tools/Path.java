c9f720442b2b0cc49c7b8ff4a3390083b4575d81
Indigoblin <axolot.jvc@gmail.com>
2020-02-20 15:20:26 +0100

    Add Tools

--- src/Tools/Path.java
+++ src/Tools/Path.java
@@ -1,4 +1,26 @@
 package Tools;
 
 public class Path {
+
+    // Police d'écriture
+    public static final String fontDTMSans = "../Asset/Fonts/DTM-Sans.otf";
+    public static final String monsterFriendFore = "../Asset/Fonts/MonsterFriendFore.otf";
+    public static final String monsterFriendBack = "../Asset/Fonts/MonsterFriendBack.otf";
+
+    // Sprite personnage non-jouable
+    public static final String doggo = "Asset/Pictures/NPC/DoggoPet.gif";
+
+    // Sprite joueur
+    public static final String joueurFullHealth = "Asset/Pictures/PC/Human_SOUL.png";
+    // joueur 20 pv damage taken
+    // joueur 15 pv
+    // joueur 15 pv damage taken
+    // joueur 10 pv
+    // joueur 10 pv damage taken
+    // joueur 5 pv
+    // joueur 5 pv damage taken
+    public static final String joueurDeath = "Asset/Pictures/PC/Death_SOUL.gif";
+
+    // Sprite attack
+    public static final String doggoSword = "Asset/Pictures/NPC/doggoSword.png";
 }
